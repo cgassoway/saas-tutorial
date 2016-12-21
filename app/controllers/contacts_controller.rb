71428de6db72e61ff05ea562cf1fbbdc9f6f633e
cgassoway <cgassoway@gmail.com>
2016-12-21 00:34:09 +0000

    Added database update for Contacts

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,7 +1,24 @@
 class ContactsController < ApplicationController
   
   def new
-    
+    #create new contact in instance variable @contact
+    # It connects with new form by using this variable
+    @contact = Contact.new
   end
+  
+  def create
+    @contact = Contact.new(contact_params)
+    if @contact.save()
+      redirect_to new_contact_path, notice: "Message sent"
+    else
+      redirect_to new_contact_path, notice: "Error occurred"
+    end
+  end
+  
+  private
+  
+    def contact_params
+      params.require(:contact).permit(:name, :email, :comments)
+    end
   
 end
