273ee39b9716327d2b547d4434770867470e4ca7
cgassoway <cgassoway@gmail.com>
2016-12-22 02:35:15 +0000

    Added html for email for contact form email

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -1,6 +1,8 @@
 class ContactMailer < ActionMailer::Base
   default to: 'fbcwg123@gmail.com'
   
+  #view folder has same name as class name(contact_mailer)
+  # view file has same name as following function (contact-email.html.erb)
   def contact_email(name, email, body)
     @name = name
     @email = email
