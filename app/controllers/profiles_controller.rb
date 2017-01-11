4f5b78fa97d9bb296c6a216dcc455eb4235225dd
cgassoway <cgassoway@gmail.com>
2017-01-11 23:35:44 +0000

    Added profile create to update profile data

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -5,6 +5,27 @@ class ProfilesController < ApplicationController
   def new
     # Render profile form
     @profile = Profile.new
-    
   end
+  
+  # POST to /users/:user_id/profile
+  def create
+    #get current user
+    @user = User.find(params[:user_id])
+    
+    # create profile linked to current User
+    @profile = @user.build_profile( profile_params)
+    if @profile.save
+      flash[:success] = "Profile successfully added"
+      redirect_to root_path
+    else
+      flash[:error] = "Unable to add profile"
+      render action: :new
+    end
+  end
+  
+  
+  private
+    def profile_params
+      params.require(:profile).permit(:first_name, :last_name, :job_title, :phone_number, :contact_email, :description)
+    end
 end
