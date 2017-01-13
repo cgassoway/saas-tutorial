7b61cabdf9b945dacf9a31d4e8c1572353a1649e
cgassoway <cgassoway@gmail.com>
2017-01-13 21:57:28 +0000

    added controller and view actions for user index and links from index to users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,6 +3,11 @@ class UsersController < ApplicationController
 
   before_action :authenticate_user!
   
+  # Get users with profiles attached to optimize database access
+  def index
+    @users = User.includes(:profile)
+  end
+  
   # GET /users/:id
   def show
     @user = User.find( params[:id])
